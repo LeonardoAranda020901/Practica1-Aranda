f64b6b7157d06d52427bde151bc8507a80b226c4
LeonardoAranda020901 <leonardoaranda6698gmail.com>
2022-09-10 10:12:15 -0500

    Aranda_Practica

---
+++ intro-typescript-pwa/src/repaso/01-tipos.datos.ts
@@ -0,0 +1,13 @@
+console.log("Hola Mundo!");
+
+let nombre:string="Cardiel";
+let cont:number=111;
+let acredita:boolean=false;
+
+let apaterno: number | string='juan';
+
+apaterno=238;
+
+var miVariable:string='Hola';
+
+const tem:number=234;
