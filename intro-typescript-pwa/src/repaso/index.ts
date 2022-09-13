94ed6a16f2d55ceb85a7dd6c9051e59353b299d0
LeonardoAranda020901 <leonardoaranda6698gmail.com>
2022-09-12 19:38:24 -0500

    Leonardo

--- intro-typescript-pwa/src/repaso/index.ts
+++ intro-typescript-pwa/src/repaso/index.ts
@@ -1,3 +1,46 @@
+interface direccionAlumno{
+    nombre:string;
+    edad:number;
+    direccion:string;
+    calle:string;
+    pais:string;
+    estado:string;
+    numero:number;
+}
+
+function AlumnoUTL(nombre:direccionAlumno)
+nombre.nombre='Mario';
+
+
 
 
 //
+
+const pruebaAlumno:utlAlumnos{
+    nombre:'Mario',
+    edad:23,
+    direccion:{
+        calle:
+        pais,
+        estado,
+        numero,
+    }
+    mostrarDireccion(){
+        return this.nombres ', '+this.direccion.estado+', '+this.direccion.pais
+    }
+}
+
+
+//interface escuelaAlumno{
+    //nombre:string;
+    //email?:string;
+   // edad:number;
+   // saludar:()=>void;
+//}
+
+//function alumnoUTL(alumno:escuelaAlumno,calificar:number):void{
+
+    //alumno.nombre='Leo';
+    //alumno.email='leo@gmail.com';
+    //alumno.edad=calificar;
+//}
